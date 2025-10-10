d9e381286467c8c273bd1e09d0efeb7409734101
CarVigil <carvigil21@gmail.com>
2025-10-10 20:00:21 -0300

    Modifico modelo de premio

--- backend-copa-pymes/src/models/premio.model.ts
+++ backend-copa-pymes/src/models/premio.model.ts
@@ -1,12 +1,13 @@
 import { Entity, Property, ManyToOne, PrimaryKey } from '@mikro-orm/core';
 import { Torneo } from './torneo.model';
+import { BaseModel } from '../shared/db/baseModel.model';
 
 @Entity()
-export class Premio {
-    @PrimaryKey()
+export class Premio extends BaseModel {
+    @Property()
     fecha_entrega!: Date;
 
-    @ManyToOne(() => Torneo, { primary: true })
+    @ManyToOne(() => Torneo)
     torneo!: Torneo;
 
     @Property()
